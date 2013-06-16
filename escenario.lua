c643c57a255d8a075b80350df5b6e639e1ee0307
amir00aranibar <amir.aranibar@gmail.com>
2013-06-16 11:13:58 -0400

    policias touch

--- escenario.lua
+++ escenario.lua
@@ -8,10 +8,26 @@
 	physics.start()
 	physics.setGravity( 0, 0 )
 
+	--posicion de polis,tiempos y transicion
+	local polisArray = {}
+	local polisTiemp = {}
+	local polisTrans = {}
+
+	--funciones math para los disparos
+	local mRound = math.round
+	local mRand = math.random
+	local mCeil = math.ceil
+	local mAtan2 = math.atan2
+	local mSqrt = math.sqrt
+	local mPi = math.pi
+
 	local grupoLocal = display.newGroup()
 	local grupoCuadros = display.newGroup()
+	local grupoManifestantes = display.newGroup()
+	local grupoArmas = display.newGroup()
 	grupoLocal:insert(display.newImage("images/background.jpg"))
 	grupoLocal:insert(grupoCuadros)
+	grupoLocal:insert(grupoArmas)
 
 
 	local cuadroImagen = {}
@@ -54,6 +70,86 @@
 
 
 
+
+	--Hace disparar al Poli en su rango de vision
+	local function dispararPoli( poli )
+		local function autoNow()
+			local i
+			for i = 1, grupoManifestantes.numChildren do
+				local manifestante = grupoManifestantes[i]
+				if manifestante ~= nil and manifestante.x ~= nil and manifestante.y ~= nil then
+					local xPos = polisArray[poli].x
+					local yPos = polisArray[poli].y
+					local x = manifestante.x; local y = manifestante.y;
+
+
+					local angleBetween = mCeil(mAtan2( (y - yPos), (x - xPos) ) * 180 / mPi) + 90
+					local sqDistance = ((x-xPos)*(x-xPos))+((y-yPos)*(y-yPos)) --a2+b2=c2
+					local dist = mSqrt(sqDistance); --Sqaure root it.
+
+					local distTime = dist/0.4
+					if distTime < 0 then distTime = distTime -distTime -distTime end
+
+
+					--If the enemy is close we shoot.
+					if dist <= 80 and dist >= 0 or dist >= -80 and dist <= 0 then
+						--Rotate the tower..
+						polisArray[poli].rotation = angleBetween-180
+
+						--Fire the weapon..
+						local shot = display.newCircle(0,3,3)
+						shot.x = xPos; shot.y = yPos;
+						shot:setFillColor(240,0,0)
+						shot.name = "weapon"; shot.rotation = angleBetween+90;
+						physics.addBody(shot, { isSensor = true } )
+						weaponGroup:insert(shot)
+
+						local function kill () timer.performWithDelay(2, function() if shot ~= nil then display.remove(shot); shot = nil; end; end,1);end
+						polisTrans[poli] = transition.to(shot, {time = distTime, x = x, y = y, onComplete = kill})
+
+						--Break out the loop if we have fired.
+						break;
+					end
+				end
+			end
+		end
+		polisTiemp[poli] = timer.performWithDelay(1200,autoNow,0)
+	end
+
+
+
+	--Evento onToch para los puestos de los polis
+	local function onTouch( event )
+		if event.phase == "ended" then
+			local hitTile = event.target
+			local poliID = hitTile.poliID
+
+
+
+			--if hitTile.poliOn == false and money >= 50 then
+			if hitTile.poliOn == false then
+				polisArray[poliID] = display.newImageRect("images/poli.png",54,54)
+				polisArray[poliID].x = hitTile.x; polisArray[poliID].y = hitTile.y
+				grupoArmas:insert(polisArray[poliID])
+
+				--Inicia esta torre para...
+				--dispararPoli( poliID )
+
+				--Descuenta Dinero para comprar Policia
+				--changeText("Dinero", 100)
+				hitTile.poliOn = true
+
+			elseif hitTile.poliOn == true then
+				--Remove tower and add 40 money..
+				hitTile.poliOn = false
+				--changeText("Dinero", -40)
+				--display.remove(polisArray[poliID]); polisArray[poliID] = nil
+			end
+		end
+		return true
+	end
+
+
 	--Dibuja el mapa de recorrido de los manifestantes y puestos de los polis
 	local function dibujar()
 		for i=1, #mapa do
@@ -94,16 +190,16 @@
 			end
 
 			--si se encuantra en el cuadro final (fn) agregar un objeto a physics.
-			if mapa[i] == "fn" then
-				cuadroImagen[i].name = "fn";
-				physics.addBody(cuadroImagen[i], { isSensor = true } )
-			end
+			--if mapa[i] == "fn" then
+				--cuadroImagen[i].name = "fn";
+				--physics.addBody(cuadroImagen[i], { isSensor = true } )
+			--end
 
 			--crea un Listener onTouch para los puestos de los polis.
 			if placementAllowed == true then
-				cuadroImagen[i].towerOn = false
-				cuadroImagen[i].towerID = i
-				cuadroImagen[i]:addEventListener("touch", levelTouched)
+				cuadroImagen[i].poliOn = false
+				cuadroImagen[i].poliID = i
+				cuadroImagen[i]:addEventListener("touch", onTouch)
 			end
 
 		end
@@ -111,6 +207,9 @@
 
 
 	dibujar()
+
+
+
 --end
 
 
